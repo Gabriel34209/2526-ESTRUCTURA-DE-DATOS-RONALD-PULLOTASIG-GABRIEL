19d1de88dd76edd22f8ab3a4e10ccfafc02c9ab0
Gabriel34209 <rg.pullotasigb@uea.edu.ec>
2026-01-11 16:43:34 -0500

    Semana5

---
+++ MiEjercicio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Crear y almacenar los números del 1 al 10
+        List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // 2. Mostrar en orden inverso
+        for (int i = numeros.Count - 1; i >= 0; i--)
+        {
+            Console.Write(numeros[i]);
+
+            // Agregar coma solo si no es el último número en mostrarse
+            if (i > 0)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+}
